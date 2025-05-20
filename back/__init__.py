82f9309289cf203c4aab573238ad13caee1ba245
Carlos Atanes Vences <carlos117g@gmail.com>
2025-05-20 12:21:47 +0000

    [FEAT] New system endpoints init.py

--- back/__init__.py
+++ back/__init__.py
@@ -5,6 +5,16 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from back.config import Config
 
+# imports from back
+from back.auth.routes import auth
+from back.character.routes import character
+from back.inventory.routes import inventory
+from back.ability.routes import ability
+from back.spell.routes import spell
+from back.condition.routes import condition
+from back.notes.routes import notes
+from back.relationship.routes import relationship
+
 db = SQLAlchemy()
 jwt = JWTManager()
 migrate = Migrate()
@@ -18,10 +28,13 @@ def create_app():
     migrate.init_app(app, db)
     CORS(app)
 
-    from back.auth.routes import auth
-    from back.character.routes import character
-
     app.register_blueprint(auth, url_prefix='/api')
     app.register_blueprint(character, url_prefix='/api')
+    app.register_blueprint(inventory, url_prefix='/api')
+    app.register_blueprint(ability, url_prefix='/api')
+    app.register_blueprint(spell, url_prefix='/api')
+    app.register_blueprint(condition, url_prefix='/api')
+    app.register_blueprint(notes, url_prefix='/api')
+    app.register_blueprint(relationship, url_prefix='/api')
 
     return app
